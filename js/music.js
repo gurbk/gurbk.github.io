675a233725a9bfd7b8adaee05f2b8ca81153a835
gurbk <110977507+gurbk@users.noreply.github.com>
2022-09-04 16:00:36 +0800

    Update music.js

--- js/music.js
+++ js/music.js
@@ -1,17 +1,12 @@
 /*
 音乐信息
-
 感谢 @武恩赐 提供的 MetingAPI
 https://api.wuenci.com/meting/api/
-
-作者: imsyy
-主页：https://www.imsyy.top/
-GitHub：https://github.com/imsyy/home
 版权所有，请勿删除
 */
-let server = "netease"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
+let server = "tencent"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
 let type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
-let id = "7452421335"; //封面 ID / 单曲 ID / 歌单 ID
+let id = "CGtBPNmz1au4"; //封面 ID / 单曲 ID / 歌单 ID
 
 $.ajax({
     url: "https://api.wuenci.com/meting/api/?server=" + server + "&type=" + type + "&id=" + id,
