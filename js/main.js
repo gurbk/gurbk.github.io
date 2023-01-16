de69316104483cbf0af8339f9e571017407d48f0
故人 <110977507+gurbk@users.noreply.github.com>
2023-01-16 19:04:43 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -128,8 +128,8 @@ $('#hitokoto').click(function () {
 //获取天气
 //请前往 https://www.mxnzp.com/doc/list 申请 app_id 和 app_secret
 //请前往 https://dev.qweather.com/ 申请 key
-const add_id = "wrknltonr0foslhs"; // app_id
-const app_secret = "Nlh1c0F6d0ZDU2pDR0J3YVBVbkhudz09"; // app_secret
+const add_id = "0jtcqrlaflmjzdpc"; // app_id
+const app_secret = "ZHhUWldRYW1PcmxNcmpraUovVXR5UT09"; // app_secret
 const key = "ec7fd505d0664f7fba1a32bdc47841eb" // key
 function getWeather() {
     fetch("https://www.mxnzp.com/api/ip/self?app_id=" + add_id + "&app_secret=" + app_secret)
